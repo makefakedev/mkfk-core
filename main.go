a6bb7f90062918d5c68f00f8a14d5fb56fa760ac
jfahne <joeyfahnestock@gmail.com>
2024-10-07 06:14:05 -0400

    feat: Init cli

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+/*
+Copyright © 2024 Make Fake Data LLC <makefakedev@proton.me>
+
+*/
 package main
 
-func main() {}
+import "github.com/makefakedev/mkfk-core/cmd"
+
+func main() {
+	cmd.Execute()
+}
