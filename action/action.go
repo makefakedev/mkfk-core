f585349eec07de943a2bc57b4f34a5fe83b8940c
jfahne <joeyfahnestock@gmail.com>
2024-10-07 06:32:10 -0400

    feat: Initial action interface

--- action/action.go
+++ action/action.go
@@ -1,1 +1,3 @@
 package action
+
+type Action interface{}
--- action/action_test.go
+++ action/action_test.go
@@ -2,4 +2,9 @@ package action
 
 import "testing"
 
-func TestAction(t *testing.T) {}
+func TestAction(t *testing.T) {
+	var testAction Action
+	if testAction != nil {
+		t.Error("test action is not nil")
+	}
+}
